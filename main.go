2f7fbd9b9ad0153f35cab61c4e5380e4489d152b
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-11-09 15:09:08 -0200

    fix: leave z-glob

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	"github.com/alecthomas/kingpin"
+	zglob "github.com/mattn/go-zglob"
 	"github.com/pmezard/go-difflib/difflib"
 )
 
@@ -16,9 +17,8 @@ var (
 	commit  = "none"
 	date    = "unknown"
 
-	app   = kingpin.New("jsonfmt", "Like gofmt, but for JSON")
-	files = app.Arg("files", "glob of the files you want to check").Strings()
-
+	app      = kingpin.New("jsonfmt", "Like gofmt, but for JSON")
+	globs    = app.Arg("files", "glob of the files you want to check").Default("**/*.json").Strings()
 	write    = app.Flag("write", "write changes to the files").Short('w').Bool()
 	indent   = app.Flag("indent", "characteres used to indend the json").Short('i').Default("  ").String()
 	failfast = app.Flag("failfast", "fail on the first error").Bool()
@@ -31,38 +31,46 @@ func main() {
 
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 
-	if len(*files) == 0 {
-		app.FatalUsage("list of files to check is empty")
-	}
-
 	var failed bool
-	for _, file := range *files {
-		bts, err := ioutil.ReadFile(file)
-		app.FatalIfError(err, "failed to read file: %s", file)
-		var out bytes.Buffer
-		err = json.Indent(&out, bytes.TrimSpace(bts), "", *indent)
-		app.FatalIfError(err, "failed to format json file: %s", file)
-		out.Write([]byte{'\n'})
-		if bytes.Equal(bts, out.Bytes()) {
-			continue
-		}
-		if *write {
-			err := ioutil.WriteFile(file, out.Bytes(), 0)
-			app.FatalIfError(err, "failed to write json file: %s", file)
-			continue
-		}
+	for _, glob := range *globs {
+		matches, err := zglob.Glob(glob)
+		app.FatalIfError(err, "failed to parse glob: %s", glob)
 
-		diff, err := difflib.GetUnifiedDiffString(difflib.UnifiedDiff{
-			A:        difflib.SplitLines(string(bts)),
-			B:        difflib.SplitLines(string(out.Bytes())),
-			FromFile: "Original",
-			ToFile:   "Formatted",
-			Context:  3,
-		})
-		app.FatalIfError(err, "failed to diff file: %s", file)
-		app.Errorf("file %s differs:\n%s\n", file, diff)
-		failed = true
-		if *failfast {
+		for _, match := range matches {
+			bts, err := ioutil.ReadFile(match)
+			app.FatalIfError(err, "failed to read file: %s", match)
+
+			var out bytes.Buffer
+			err = json.Indent(&out, bytes.TrimSpace(bts), "", *indent)
+			app.FatalIfError(err, "failed to format json file: %s", match)
+			out.Write([]byte{'\n'})
+
+			if bytes.Equal(bts, out.Bytes()) {
+				continue
+			}
+
+			if *write {
+				err := ioutil.WriteFile(match, out.Bytes(), 0)
+				app.FatalIfError(err, "failed to write json file: %s", match)
+				continue
+			}
+
+			diff, err := difflib.GetUnifiedDiffString(difflib.UnifiedDiff{
+				A:        difflib.SplitLines(string(bts)),
+				B:        difflib.SplitLines(string(out.Bytes())),
+				FromFile: "Original",
+				ToFile:   "Formatted",
+				Context:  3,
+			})
+			app.FatalIfError(err, "failed to diff file: %s", match)
+			app.Errorf("file %s differs:\n%s\n", match, diff)
+
+			failed = true
+			if *failfast {
+				break
+			}
+		}
+		if failed && *failfast {
 			break
 		}
 	}
