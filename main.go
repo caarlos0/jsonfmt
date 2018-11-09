7fe190b9aca3d24499b975133939bb6d738fb9c8
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-11-09 10:10:58 -0200

    fix: improvements

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ var (
 	date    = "unknown"
 
 	app   = kingpin.New("jsonfmt", "Like gofmt, but for JSON")
-	files = app.Flag("files", "glob of the files you want to check").Short('f').Default("**/*.json").String()
 	write = app.Flag("write", "write changes to the files").Short('w').Bool()
+	globs = app.Arg("files", "glob of the files you want to check").Default("**/*.json").Strings()
 )
 
 func main() {
@@ -29,39 +29,41 @@ func main() {
 
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 
-	matches, err := zglob.Glob(*files)
-	app.FatalIfError(err, "failed to parse glob: %s", *files)
-
 	var failed bool
-	for _, match := range matches {
-		file, err := os.Open(match)
-		app.FatalIfError(err, "failed to open file: %s", match)
-		defer file.Close()
-		bts, err := ioutil.ReadAll(file)
-		app.FatalIfError(err, "failed to read file: %s", match)
-		_ = file.Close()
+	for _, glob := range *globs {
+		matches, err := zglob.Glob(glob)
+		app.FatalIfError(err, "failed to parse glob: %s", glob)
 
-		var tmp json.RawMessage
-		app.FatalIfError(json.Unmarshal(bts, &tmp), "failed to parse json file: %s", match)
-		out, err := json.MarshalIndent(tmp, "", "  ")
-		app.FatalIfError(err, "failed to parse json file: %s", match) // XXX: improve error msg
-		out = append(out, '\n')
-		if !bytes.Equal(bts, out) {
-			if *write {
-				app.FatalIfError(ioutil.WriteFile(match, out, 0), "failed to write json file: %s", match)
-				continue
+		for _, match := range matches {
+			file, err := os.Open(match)
+			app.FatalIfError(err, "failed to open file: %s", match)
+			defer file.Close()
+			bts, err := ioutil.ReadAll(file)
+			app.FatalIfError(err, "failed to read file: %s", match)
+			_ = file.Close()
+
+			var tmp json.RawMessage
+			app.FatalIfError(json.Unmarshal(bts, &tmp), "failed to parse json file: %s", match)
+			out, err := json.MarshalIndent(tmp, "", "  ")                 // TODO: support to customize indent
+			app.FatalIfError(err, "failed to parse json file: %s", match) // XXX: improve error msg
+			out = append(out, '\n')
+			if !bytes.Equal(bts, out) {
+				if *write {
+					app.FatalIfError(ioutil.WriteFile(match, out, 0), "failed to write json file: %s", match)
+					continue
+				}
+
+				diff, err := difflib.GetUnifiedDiffString(difflib.UnifiedDiff{
+					A:        difflib.SplitLines(string(bts)),
+					B:        difflib.SplitLines(string(out)),
+					FromFile: "Original",
+					ToFile:   "Formatted",
+					Context:  3,
+				})
+				app.FatalIfError(err, "failed to diff file: %s", match)
+				app.Errorf("file %s differs:\n%s\n", match, diff)
+				failed = true
 			}
-
-			diff, err := difflib.GetUnifiedDiffString(difflib.UnifiedDiff{
-				A:        difflib.SplitLines(string(bts)),
-				B:        difflib.SplitLines(string(out)),
-				FromFile: "Original",
-				ToFile:   "Formatted",
-				Context:  3,
-			})
-			app.FatalIfError(err, "failed to diff file: %s", match)
-			app.Errorf("file %s differs:\n%s\n", match, diff)
-			failed = true
 		}
 	}
 	if failed {
