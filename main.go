935aea33c55069b5bf2a7ddcb4733313e99f50ec
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-11-09 14:58:38 -0200

    refactor: remove zglob

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"os"
 
 	"github.com/alecthomas/kingpin"
-	zglob "github.com/mattn/go-zglob"
 	"github.com/pmezard/go-difflib/difflib"
 )
 
@@ -19,7 +18,7 @@ var (
 
 	app   = kingpin.New("jsonfmt", "Like gofmt, but for JSON")
 	write = app.Flag("write", "write changes to the files").Short('w').Bool()
-	globs = app.Arg("files", "glob of the files you want to check").Default("**/*.json").Strings()
+	files = app.Arg("files", "glob of the files you want to check").Strings()
 )
 
 func main() {
@@ -29,38 +28,37 @@ func main() {
 
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 
+	if len(*files) == 0 {
+		app.FatalUsage("list of files to check is empty")
+	}
+
 	var failed bool
-	for _, glob := range *globs {
-		matches, err := zglob.Glob(glob)
-		app.FatalIfError(err, "failed to parse glob: %s", glob)
-
-		for _, match := range matches {
-			bts, err := ioutil.ReadFile(match)
-			app.FatalIfError(err, "failed to read file: %s", match)
-			var out bytes.Buffer
-			err = json.Indent(&out, bytes.TrimSpace(bts), "", "  ") // TODO: support to customize indent
-			app.FatalIfError(err, "failed to format json file: %s", match)
-			out.Write([]byte{'\n'})
-			if bytes.Equal(bts, out.Bytes()) {
-				continue
-			}
-			if *write {
-				err := ioutil.WriteFile(match, out.Bytes(), 0)
-				app.FatalIfError(err, "failed to write json file: %s", match)
-				continue
-			}
-
-			diff, err := difflib.GetUnifiedDiffString(difflib.UnifiedDiff{
-				A:        difflib.SplitLines(string(bts)),
-				B:        difflib.SplitLines(string(out.Bytes())),
-				FromFile: "Original",
-				ToFile:   "Formatted",
-				Context:  3,
-			})
-			app.FatalIfError(err, "failed to diff file: %s", match)
-			app.Errorf("file %s differs:\n%s\n", match, diff)
-			failed = true
+	for _, file := range *files {
+		bts, err := ioutil.ReadFile(file)
+		app.FatalIfError(err, "failed to read file: %s", file)
+		var out bytes.Buffer
+		err = json.Indent(&out, bytes.TrimSpace(bts), "", "  ") // TODO: support to customize indent
+		app.FatalIfError(err, "failed to format json file: %s", file)
+		out.Write([]byte{'\n'})
+		if bytes.Equal(bts, out.Bytes()) {
+			continue
 		}
+		if *write {
+			err := ioutil.WriteFile(file, out.Bytes(), 0)
+			app.FatalIfError(err, "failed to write json file: %s", file)
+			continue
+		}
+
+		diff, err := difflib.GetUnifiedDiffString(difflib.UnifiedDiff{
+			A:        difflib.SplitLines(string(bts)),
+			B:        difflib.SplitLines(string(out.Bytes())),
+			FromFile: "Original",
+			ToFile:   "Formatted",
+			Context:  3,
+		})
+		app.FatalIfError(err, "failed to diff file: %s", file)
+		app.Errorf("file %s differs:\n%s\n", file, diff)
+		failed = true
 	}
 	if failed {
 		app.Fatalf("some files are not properly formated, check above")
